c2165ec02c8bd9467ba7501dbfc805846d8d41eb
Wutasun <wutasun@gmail.com>
2026-03-05 22:45:55 +0800

    Delete tasun-guard-v2.js

--- tasun-guard-v2.js
+++
@@ -1,8 +0,0 @@
-/* tasun-guard-v2.js */
-(function(){
-  try{
-    if(window.tasunAuth && typeof window.tasunAuth.requireLogin === "function"){
-      window.tasunAuth.requireLogin();
-    }
-  }catch(e){}
-})();
