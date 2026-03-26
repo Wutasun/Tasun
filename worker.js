d82d2a8bb6fb2b882575646ceb812875ccb9efab
Wutasun <wutasun@gmail.com>
2026-03-26 15:26:05 +0800

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,6 @@
+// Legacy placeholder for old /Tasun/worker.js requests
+// Prevents 404 noise from older pages or cached scripts.
+(function () {
+  console.info('[legacy worker placeholder] loaded');
+  window.__TASUN_LEGACY_WORKER_PLACEHOLDER__ = true;
+})();
